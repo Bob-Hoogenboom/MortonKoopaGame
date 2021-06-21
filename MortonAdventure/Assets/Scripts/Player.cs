a4105025dcc43f5a671850a0506ba8ec00ebfb0c
Bob Hoogenboom <55579218+Bob-Hoogenboom@users.noreply.github.com>
2021-06-21 13:57:08 +0200

    NewEmptyUnityProject
    switched from unity 2020 to 2019 for stability.

---
+++ MortonAdventure/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+/*    [SerializeField] private float _MoveSpeed;
+    [SerializeField] private float _Walkspeed;
+    [SerializeField] private float _RunSpeed;
+
+    [SerializeField] private Vector3 _MoveDirection;
+
+    private CharacterController _PlayerController;
+
+
+    private void Start() {
+        _PlayerController = GetComponent<CharacterController>();
+    }
+
+    private void Update() {
+        Moving();
+    }
+
+    private void Moving() {
+        float moveZ = Input.GetAxis("vertical");
+
+        _MoveDirection = new Vector3(0,0, moveZ);
+        _MoveDirection *= _Walkspeed;
+
+        _PlayerController.Move(_MoveDirection * Time.deltaTime);
+    }*/
+}
